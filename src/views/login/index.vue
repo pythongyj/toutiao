b81b1e6a52803fa7bca8c6ea06f152513d906386
YunjieGe <1329712473@qq.com>
2019-12-18 21:45:33 +0800

    页面布局补充

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,23 +5,36 @@
         <img src="..//../assets/img/logo_index.png" alt />
       </div>
       <el-form>
-          <el-form-item>
-              <el-input></el-input>
-          </el-form-item>
-          <el-form-item>
-              <el-input class="loginCode"></el-input>
-              <el-button class="loginGetCode" plain>获取验证码</el-button>
-          </el-form-item>
-          <el-form-item>
-              <el-button type="primary" class="submint">登录</el-button>
-          </el-form-item>
+        <el-form-item>
+          <el-input v-model="formData.phone" placeholder="请输入您的手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="formData.code" class="loginCode" placeholder="请输入您的验证码"></el-input>
+          <el-button class="loginGetCode" plain>获取验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="formData.reads">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="submint">登录</el-button>
+        </el-form-item>
       </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      formData: {
+        phone: '',
+        code: '',
+        reads: true
+      }
+    }
+  }
+}
 </script>
 
 <style lang='less' scope>
@@ -32,23 +45,23 @@ export default {}
   justify-content: center;
   align-items: center;
   .loginCard {
-    width: 400px;
-    height: 300px;
-    .logo{
-        text-align: center;
-        margin-bottom: 30px;
-        img{
-            height: 40px;
-        }
+    width: 450px;
+    height: 350px;
+    .logo {
+      text-align: center;
+      margin-bottom: 30px;
+      img {
+        height: 40px;
+      }
     }
-    .loginCode{
-        width: 60%;
+    .loginCode {
+      width: 70%;
     }
-    .loginGetCode{
-        float: right;
+    .loginGetCode {
+      float: right;
     }
-    .submint{
-        width: 100%;
+    .submint {
+      width: 100%;
     }
   }
 }
