322c213bbf2fa4eaf43739f6f472cec640c8b066
YunjieGe <1329712473@qq.com>
2019-12-18 21:35:31 +0800

    login页面布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home'
+import Login from '../views/login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/Login'
+  },
+  {
+    path: '/home',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
   // {
   //   path: '/about',
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,55 @@
 <template>
-  <h2>我是login</h2>
+  <div class="loginImg">
+    <el-card class="loginCard">
+      <div class="logo">
+        <img src="..//../assets/img/logo_index.png" alt />
+      </div>
+      <el-form>
+          <el-form-item>
+              <el-input></el-input>
+          </el-form-item>
+          <el-form-item>
+              <el-input class="loginCode"></el-input>
+              <el-button class="loginGetCode" plain>获取验证码</el-button>
+          </el-form-item>
+          <el-form-item>
+              <el-button type="primary" class="submint">登录</el-button>
+          </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' scope>
+.loginImg {
+  background-image: url("../../assets/img/login_bg.jpg");
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .loginCard {
+    width: 400px;
+    height: 300px;
+    .logo{
+        text-align: center;
+        margin-bottom: 30px;
+        img{
+            height: 40px;
+        }
+    }
+    .loginCode{
+        width: 60%;
+    }
+    .loginGetCode{
+        float: right;
+    }
+    .submint{
+        width: 100%;
+    }
+  }
+}
 </style>
