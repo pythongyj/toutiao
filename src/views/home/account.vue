56360860d7981dcccbf77bbf0a2264b43cbc8066
YunjieGe <1329712473@qq.com>
2019-12-30 11:32:06 +0800

    用户信息保存完成

--- src/views/home/account.vue
+++ src/views/home/account.vue
@@ -1,16 +1,106 @@
 <template>
   <el-card>
     <bread slot="header">
-      <template slot="title">
-        账户信息
-      </template>
+      <template slot="title">账户信息</template>
     </bread>
+    <el-row type="flex" justify="space-around">
+      <el-form ref="myForm" :model="formData" :rules="rules" class="a" label-width="100px">
+        <el-form-item prop="name" label="用户名">
+          <el-input v-model="formData.name"></el-input>
+        </el-form-item>
+        <el-form-item prop="intro" label="简介">
+          <el-input v-model="formData.intro"></el-input>
+        </el-form-item>
+        <el-form-item prop="email" label="邮箱">
+          <el-input v-model="formData.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" >
+          <el-input disabled v-model="formData.mobile"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="save">保存信息</el-button>
+        </el-form-item>
+      </el-form>
+      <el-upload action :show-file-list="false" :http-request="uploadImg" class="upImg">
+        <img :src="formData.photo ? formData.photo : defaultImg" alt />
+      </el-upload>
+    </el-row>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      defaultImg: require('../../assets/img/login_bg3.jpg'),
+      formData: {
+        name: '',
+        intro: '',
+        photo: '',
+        email: '',
+        mobile: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入用户名' }
+        ],
+        intro: [
+          { required: true, message: '请输入简介' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱' }
+        ]
+      }
+    }
+  },
+  methods: {
+    save () {
+      this.$refs.myForm.validate().then(result => {
+        if (result) {
+          this.$axios({
+            url: '/user/profile',
+            method: 'PATCH',
+            data: this.formData
+          }).then(result => {
+            this.$message({
+              type: 'success',
+              message: '修改成功'
+            })
+            this.getProfile()
+          })
+        }
+      })
+    },
+    uploadImg () {},
+    getProfile () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+        console.log(this.formData)
+      })
+    }
+  },
+  created () {
+    this.getProfile()
+  }
+}
 </script>
 
-<style>
+<style lang="less" scoped>
+.upImg {
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    margin-top: 30px;
+  }
+
+}
+  .el-form-item {
+    width: 500px;
+    .el-input {
+      width: 100%;
+    }
+  }
 </style>
